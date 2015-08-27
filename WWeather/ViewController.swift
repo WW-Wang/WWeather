926f0f79d74bdd2d6292da47b9c78209aa5c71fb
WW-Wang <wzh464686744@gmail.com>
2015-08-27 14:35:07 +0800

    初始化

---
+++ WWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWeather
+//
+//  Created by zh.wang on 15/8/27.
+//  Copyright (c) 2015年 WW_Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
